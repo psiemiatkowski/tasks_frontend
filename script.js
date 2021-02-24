f654445542ba8d7bda9494e690a14f55e17c384d
psiemiatkowski <psiemiatkowski@gmail.com>
2021-02-24 14:37:35 +0100

    js not index

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'https://morning-spire-01286.herokuapp.com/v1/';
+  const trelloApiRoot = 'https://morning-spire-01286.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
